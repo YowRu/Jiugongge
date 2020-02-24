b98cb3a695f7a20a1b2352c2d571d9dc6fd071e0
Angela <love_3221213@hotmail.com>
2020-02-24 23:57:29 +0800

    Updates

---
+++ precache-manifest.6fa72861f4697dd6cd45fb32f5a6baa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c336a0060694953850d2da2e1fd6d3c",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "dc03252ea7ae9e6486ec",
+    "url": "/my-app/static/css/main.c4af5fa5.chunk.css"
+  },
+  {
+    "revision": "013914e61c074d7fc6cf",
+    "url": "/my-app/static/js/2.377fad85.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-app/static/js/2.377fad85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc03252ea7ae9e6486ec",
+    "url": "/my-app/static/js/main.38894930.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  }
+]);
