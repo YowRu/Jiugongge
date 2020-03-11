89fb65677035e937e89d5ad55a8509aab4f3077a
Angela <love_3221213@hotmail.com>
2020-03-12 00:02:42 +0800

    Recode RadomColor

--- src/App/RadomColor.js
+++ src/App/RadomColor.js
@@ -1,53 +1,55 @@
-import React ,{ useEffect }from 'react'
+import React from 'react'
+import * as R from 'ramda'
 
 function RadomColor({ isRadomPage }){
-    const init = () => {
-        //顏色字串
-        let colorStr=''
-        function color () { 
-            const r = function () { return Math.floor(Math.random()*256) }
-            return colorStr = 'rgb(' + r() +',' + r() +',' + r() +')'
-        }
-        const Height = window.screen.height * 0.8
-        const Width = window.screen.width
-        const ulHeight = (Height - 110 )/8
-        const ulWidth = Width * 0.9
-        const liWidth = (ulWidth * 0.9)/8
-        //ul
-          for (let i=1;i<9;i++) { 
-              const ul = document.createElement('ul')
-              document.querySelector('.RadomColor').appendChild(ul).style.width = ulWidth + 'px'      
-          }
-        //li 
-          for (let j=0;j<8;j++) {
-    
-              var x = document.querySelector('.RadomColor').querySelectorAll('ul');
-              for (let k=0;k<x.length;k++) {
-                  let li = document.createElement('li')
-                  color()
-                  document.querySelector('.RadomColor').querySelectorAll('ul')[k].appendChild(li).style.height = ulHeight + 'px' 
-                  document.querySelector('.RadomColor').querySelectorAll('ul')[k].appendChild(li).style.width = liWidth + 'px' 
-                  document.querySelector('.RadomColor').querySelectorAll('ul')[k].appendChild(li).style.backgroundColor=colorStr
-              }
-          }
-          
-          
-        //click 
-          document.querySelector('.RadomColor').addEventListener ('click', function(event) {
-              var targetElement = event.srcElement.localName
-    
-              if (targetElement === 'li') {
-                  color()
-                  event.srcElement.style.backgroundColor = colorStr
-              }
-          })
-      }
+
+      const hasData = R.compose(
+        R.not,
+        R.either(R.isNil, R.isEmpty)
+      ) 
       
-      useEffect(()=>{
-        init()
-      },[])
+      const BOX_NUMBER = 64
+
+      let colorStr
+      function color () { 
+          const r = function () { return Math.floor(Math.random()*256) }
+          return colorStr = `rgb(${r()},${r()},${r()})`
+      }
+
+      let data =[]
+      for(let i = 0; i < BOX_NUMBER; i ++) {
+        color()
+        const item = {}
+        item['id'] = i
+        item['color'] = colorStr
+        data.push(item)
+      }
+
+      const changeColor = id => {
+        color()
+        const item = {}
+        item['id'] = id
+        item['color'] = colorStr
+        data.splice(id, 1, item)
+        console.log('click')
+        return data
+      }
+
       return (
-        <div className='RadomColor' style={{display: isRadomPage ? 'block' : 'none'}}></div>
+          <div className='RadomColor' style={{display: isRadomPage ? 'flex' : 'none'}}>
+            {hasData(data) && data.map(item => (
+                <div
+                  className = "radomDiv"
+                  key = {item.id}
+                  id = {item.id}
+                  style = {{backgroundColor: item.color }}
+                  onClick = {() => {
+                    changeColor(item.id)
+                  }}
+                />
+            ))}
+          </div>
+      
       )
 }
 
