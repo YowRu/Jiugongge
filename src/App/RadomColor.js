1bd7456979194fa1e15d7a6f2222025703b36849
Angela Yu <angela.yu@yjtech.tw>
2020-03-27 00:32:46 +0800

    Recode RadomColor.js

--- src/App/RadomColor.js
+++ src/App/RadomColor.js
@@ -1,56 +1,65 @@
-import React from 'react'
+import React, { useState, useMemo } from 'react'
 import * as R from 'ramda'
 
 function RadomColor({ isRadomPage }){
+  const hasData = R.compose(
+    R.not,
+    R.either(R.isNil, R.isEmpty)
+  )
+  
+  const BOX_NUMBER = 64
+  let box = []
+  const [data,setData] = useState(box)
+  
+  function fetchColor () { 
+    const r = () => Math.floor(Math.random()*256)
+    return `rgb(${r()},${r()},${r()})`
+  }
 
-      const hasData = R.compose(
-        R.not,
-        R.either(R.isNil, R.isEmpty)
-      ) 
-      
-      const BOX_NUMBER = 64
-
-      let colorStr
-      function color () { 
-          const r = function () { return Math.floor(Math.random()*256) }
-          return colorStr = `rgb(${r()},${r()},${r()})`
+  const init = () => {
+    for(let i = 0; i < BOX_NUMBER; i ++) {
+      const item = {
+        id: i,
+        color: fetchColor()
       }
+      box.push(item)
+    }
+    setData(box)
+  }
 
-      let data =[]
-      for(let i = 0; i < BOX_NUMBER; i ++) {
-        color()
-        const item = {}
-        item['id'] = i
-        item['color'] = colorStr
-        data.push(item)
+  useMemo(() => {
+    init()
+  },[])
+
+  const changeColor = (id) => {
+    try {
+      const item = {
+        'id': id,
+        'color': fetchColor()
       }
+      const components = data.slice(0)
+      components.splice(id,1,item)
+      setData(components)
+    } catch (err) {
+      window.alert(err.message)
+    }
+  }
 
-      const changeColor = id => {
-        color()
-        const item = {}
-        item['id'] = id
-        item['color'] = colorStr
-        data.splice(id, 1, item)
-        console.log('click')
-        return data
-      }
-
-      return (
-          <div className='RadomColor' style={{display: isRadomPage ? 'flex' : 'none'}}>
-            {hasData(data) && data.map(item => (
-                <div
-                  className = "radomDiv"
-                  key = {item.id}
-                  id = {item.id}
-                  style = {{backgroundColor: item.color }}
-                  onClick = {() => {
-                    changeColor(item.id)
-                  }}
-                />
-            ))}
-          </div>
-      
-      )
+  return (
+      <div className = 'RadomColor' style = {{display: isRadomPage ? 'flex' : 'none'}}>
+        {hasData(data) && data.map(item => (
+          <div
+            className = "radomDiv"
+            key = {item.id}
+            id = {item.id}
+            style = {{backgroundColor: item.color}}
+            onClick = {() => {
+              changeColor(item.id)
+            }}
+          />
+        ))}
+      </div>
+  )
 }
 
 export default RadomColor
